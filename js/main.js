549ebe9ae3d58b78855f53e900b63a7485571d14
Esing <91776041+Zxis233@users.noreply.github.com>
2026-06-08 23:00:54 +0800

    Commit Hash

--- js/main.js
+++ js/main.js
@@ -4,6 +4,17 @@
  */
 
 import { initApp } from './events.js';
+import { COMMIT_HASH } from './version.js';
+
+function renderCommitHash() {
+  const el = document.getElementById("commit-hash");
+  if (!el || !COMMIT_HASH) {
+    return;
+  }
+  el.textContent = COMMIT_HASH;
+  el.title = `Commit ${COMMIT_HASH}`;
+}
 
 // 启动应用
+renderCommitHash();
 initApp();
